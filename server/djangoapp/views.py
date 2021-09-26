4efc1f396256a8148143b6b6f38d9702843b1db0
ncontrerass <50805848+ncontrerass@users.noreply.github.com>
2021-09-26 17:39:35 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -65,7 +65,7 @@ def registration_request(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
-        url = "your-cloud-function-domain/dealerships/dealer-get"
+        url = "nathalia-djangoapp.mybluemix.net/dealerships/dealer-get"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
