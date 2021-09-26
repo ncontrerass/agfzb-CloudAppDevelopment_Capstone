a5377dfe9958f025d69b86c3cc043f3029b3d620
ncontrerass <50805848+ncontrerass@users.noreply.github.com>
2021-09-26 15:37:06 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,16 +10,16 @@ urlpatterns = [
     # name the URL
     # path(route='', view=views.index, name='index'),
     # path for about view
-    path(route='', view=views.about, name='about'),
+    path(route='about', view=views.about, name='about'),
     # path for contact us view
 
-    path(route='', view=views.contact, name='contact'),
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
-    path(route='', view=views.registration_request, name='register'),
+    path(route='register', view=views.registration_request, name='register'),
     # path for login
-    path(route='', view=views.login_request, name='login'),
+    path(route='login', view=views.login_request, name='login'),
     # path for logout
-    path(route='', view=views.logout_request, name='logout'),
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
